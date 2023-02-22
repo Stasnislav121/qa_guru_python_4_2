56480c2bf87fc1309e1c7e92238f1102574dc147
Stasnislav121 <oz3r@mail.ru>
2023-02-22 21:09:18 +0300

    Добавлены отступы

--- test_simple.py
+++ test_simple.py
@@ -2,11 +2,13 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+
 @pytest.fixture(scope='session')
 def browser_setup():
     browser.config.window_height = 1440
     browser.config.window_width = 768
     browser.open('https://google.com')
+
 
 @pytest.fixture()
 def input_clear():
@@ -16,6 +18,7 @@ def input_clear():
 def test_search_true(browser_setup):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
 
 def test_search_neg(browser_setup, input_clear):
     browser.element('[name="q"]').should(be.blank).type('e32e2dewf314r2rfd3w').press_enter()
